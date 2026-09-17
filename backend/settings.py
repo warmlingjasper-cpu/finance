aea13d3d1cb9f92735d5c055eab33e09799b1ce0
Lucas Warmling Jasper <warmlingjasper@gmail.com>
2026-09-17 01:49:00 +0100

    Configure static root

--- backend/settings.py
+++ backend/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
