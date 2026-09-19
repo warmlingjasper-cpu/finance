b0fca05bcb416c6a96ec1878a6118b43b16e5b59
Lucas Warmling Jasper <warmlingjasper@gmail.com>
2026-09-19 01:23:36 +0100

    fix allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     "finance-backend-4a93.onrender.com",
     "127.0.0.1", 
     "localhost",
+    "finance-backend-4a93.onrender.com",
 ]
 
 
@@ -141,4 +142,5 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "finance-backend-4a93.onrender.com",
 ]
